ce622f0979f5bbf3303d0db1000cfec0a9c96475
Phillipe <phillipe_jarnefors@hotmail.com>
2023-09-07 11:00:42 +0200

    Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,17 +5,16 @@ import { Icon } from '@iconify/react';
 export default function Footer() {
   return (
   <footer id="footer" className="relative mt-20 w-[100%]">
-    <h1 className="text-primary text-2xl mb-32 px-6 md:px-10 lg:px-32 2xl:px-96">Get in touch! &#128075;</h1>
-    <div className=" outline outline-5 outline-font rounded-lg absolute right-0 left-0 bottom-32 sm:bottom-40 md:bottom-60 lg:bottom-80 mt-6 mx-6 md:mx-10 lg:mx-32 2xl:mx-96">
-      <form className="grid grid-cols-5 gap-3 px-3 pb-8 pt-3 text-font">
+    <div className=" outline outline-5 outline-font rounded-lg absolute right-0 left-0 bottom-24 sm:bottom-40 md:bottom-60 lg:bottom-80 mt-6 mx-6 sm:mx-20 md:mx-30 lg:mx-60 xl:mx-96">
+      <form className="grid grid-cols-5 gap-3 px-3 sm:p-8 md:p-12 pb-8 pt-3 text-font">
         <div className="col-span-3 flex items-end">
            <input className=" bg-background w-full outline outline-1 outline-font rounded p-2"  type="text" name="" placeholder="Name" />
         </div>
         <div className="col-span-1"></div>
-        <Icon className="col-span-1 h-full w-full" icon="icon-park-outline:stamp" color="#ababab" />
+        <Icon className="col-span-1 h-full w-full rotate-6 " icon="icon-park-outline:stamp" color="#ababab" />
         <input className="col-span-3 bg-background outline outline-1 outline-font rounded p-2" type="text" name="" placeholder="Subject" />
-        <div className="col-span-5 pr-1 flex items-center justify-between">
-          <textarea className="resize-none w-[80%] bg-background outline outline-1 outline-font rounded p-2" name="message" placeholder="Message"></textarea>
+        <div className="col-span-5 pr-1 sm:pr-4 flex items-center justify-between">
+          <textarea className="resize-none w-[80%] bg-background outline outline-1 outline-font rounded p-2 " name="message" placeholder="Message"></textarea>
           <button type="submit" className="outline outline-primary w-smallBtn h-smallBtn rounded-full flex items-center justify-center">
             <Icon className="w-6 h-6" icon="bi:send" color="#61dbfb" />
           </button>
@@ -28,8 +27,13 @@ export default function Footer() {
   </footer>
   )
 }
+
+
+
 {/* <Icon className="h-logo w-logo" icon="bi:github" color="#61dbfb" />
 <Icon className="h-logo w-logo" icon="mdi:linkedin" color="#61dbfb" /> */}
 
+{/* <h1 className="text-primary text-2xl mb-24 sm:mb-2 md:mb-0 mx-6 sm:mx-20 md:mx-30 lg:mx-40 2xl:mx-96">Get in touch! &#128075;</h1> */}
 // style={{backgroundImage: `url(${phillipe})`}} 
 // 
+// <h1 className="text-primary text-2xl mb-24 sm:mb-2 md:mb-0 mx-6 sm:mx-20 md:mx-30 lg:mx-40 2xl:mx-96">Get in touch! &#128075;</h1>
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,17 +1,20 @@
 import React from 'react'
-import Milo from "../images/milo.jpg"
+import Milo from "../images/milo_rounded.jpg"
+import { Icon } from '@iconify/react/dist/iconify.js'
+
 
 export default function About() {
   return (
-    <div id='about' className='mb-32'>   
-        <h2 className="text-3xl text-primary mt-20 sm:mt-28 sm:pb-4 md:mt-40">About</h2>
+    <div id='about' className='mb-10'>   
+        <h2 className="text-3xl text-primary mt-20 sm:mt-28  md:mt-40">About</h2>
         <div className="h-2 bg-primary rounded mt-3 mb-8 sm:mb-12"></div>
-        <section className='grid grid-cols-3 items-center'>
-            <div className='col-span-2'>
+        <section className='flex flex-col bg-abstract rounded-xl px-6 py-6 sm:mx-12 sm:py-12 md:mx-20 2xl:mx-36'>
+            <div className='sm:mx-10'>
+            <Icon className='h-10 w-10' icon="lucide:quote" color='#61DBFB' />
                 <p className="text-standard text-font mt-5 sm:mt-10 ">I have a background in sheet metalwork, with over a decade of experience in the field. Over the past three years, I've dedicated my time to renovating my house. Now, I'm embarking on a new adventure in life as I pursue a career as a full-stack web developer. I share my home in Lidköping with my faithful dog, Milo, and my wonderful girlfriend, Linda.</p>
             </div>
-            <div className='col-span-1'>
-                <img className='rounded-full' src={Milo}/>
+            <div className='mt-10 flex items-center justify-center'>
+                <img className='rounded-full h-40 w-40 sm:h-52 sm:w-52 md:h-60 md:w-60 lg:h-72 lg:w-72' src={Milo}/>
             </div>
         </section>
     </div>
